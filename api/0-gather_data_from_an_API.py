703e1efb54868bdc764f8184338afff4cc555003
mutaganzwa-desire <d.mutaganzw@alustudent.com>
2025-06-29 18:22:42 +0200

    File altered

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+Script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress
+"""
+
 import json
 import requests
 from sys import argv
